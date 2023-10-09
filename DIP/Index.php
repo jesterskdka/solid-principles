7e55e747e682841743134fec7cdade783e6f3971
jesterskdka <145336684+jesterskdka@users.noreply.github.com>
2023-10-09 17:42:45 +0800

    Create Index.php

---
+++ DIP/Index.php
@@ -0,0 +1,16 @@
+<?php
+ 
+ class UserDB {
+ 
+  		private $dbConnection;
+        
+        public function _construct(MySQLConnection $dbConnection){
+          $this -> $dbConnection = $dConnection;
+        }
+          
+        public function store(User $user){
+        
+        }
+      }
+      
+?>
